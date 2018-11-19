f6a98528fdc27eabdcfcb0a181bf375edc9cd738
Luke Karlovich <luke.karlovich@colorado.edu>
2018-11-18 17:46:54 -0700

    added most recent project

--- contact.php
+++ contact.php
@@ -4,13 +4,13 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'luke.karlovich@gmail.com';
+$from = 'luke.karlovich@colorado.edu';
 
 // an email address that will receive the email with the output of the form
 $sendTo = 'luke.karlovich@gmail.com';
 
 // subject of the email
-$subject = 'New message from lukes portfolio page';
+$subject = 'New message from Lukes portfolio page';
 
 // form field names and their translations.
 // array variable name => Text to appear in the email
